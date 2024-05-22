a69fe3ba78ad93a9060d63e999945da3330ec6f7
PedroF <pfri258@gmail.com>
2024-05-22 16:56:50 +0100

    Material changes added

--- js/main-script.js
+++ js/main-script.js
@@ -29,8 +29,12 @@ var ripple1, ripple2, ripple3
 var flatcircle1, flatcircle2, flatcircle3
 var klein1, klein2, klein3
 
+var objects
 var clock = new THREE.Clock();
-var temporary_material1, temporary_material2, temporary_material3, temporary_material4, temporary_material5
+var diffuse_material1, diffuse_material2, diffuse_material3, diffuse_material4, diffuse_material5
+var phong_material1, phong_material2, phong_material3, phong_material4, phong_material5
+var cartoon_material1, cartoon_material2, cartoon_material3, cartoon_material4, cartoon_material5
+var normal_material1, normal_material2, normal_material3, normal_material4, normal_material5
 
 /////////////////////
 /* CREATE SCENE(S) */
@@ -39,7 +43,6 @@ function createScene(){
     'use strict';
 
     scene = new THREE.Scene();
-    scene.add(new THREE.AxesHelper(10));
 }
 
 //////////////////////
@@ -67,7 +70,7 @@ function createLights() {
     directionalLight.position.set(5, 50, 15);
     scene.add(directionalLight);
 
-    const ambientLight = new THREE.AmbientLight(0xffa500, 0.5);
+    const ambientLight = new THREE.AmbientLight(0xffa500, 0.1);
     scene.add(ambientLight);
 }
 
@@ -76,19 +79,15 @@ function createLights() {
 ////////////////////////
 function createObjects(){
     'use strict';
-    temporary_material1 = new THREE.MeshStandardMaterial({color: 'Gray'});
-    temporary_material2 = new THREE.MeshStandardMaterial({color: 'Yellow'});
-    temporary_material3 = new THREE.MeshStandardMaterial({color: 'Red'});
-    temporary_material4 = new THREE.MeshStandardMaterial({color: 'Blue'});
-    temporary_material5 = new THREE.MeshStandardMaterial({color: 'Purple'});
-    temporary_material5.side = THREE.DoubleSide;
+
+    createMaterials();
 
     var extrudesettings = {
         depth: 3,
         bevelEnabled: false
     };
     var main_cylinder_geometry = new THREE.CylinderGeometry(5, 5, 50, 32);
-    main_cylinder = new THREE.Mesh(main_cylinder_geometry, temporary_material1);
+    main_cylinder = new THREE.Mesh(main_cylinder_geometry, diffuse_material1);
     main_cylinder.position.set(0, 25, 0);
     main_cylinder.rotation.set(0, 0, 0);
 
@@ -98,7 +97,7 @@ function createObjects(){
     holePath.absarc(0, 0, 5, 0, Math.PI * 2, true);
     disc1_shape.holes.push(holePath);
     var disc1_geometry = new THREE.ExtrudeGeometry(disc1_shape, extrudesettings);
-    disc1 = new THREE.Mesh(disc1_geometry, temporary_material2);
+    disc1 = new THREE.Mesh(disc1_geometry, diffuse_material2);
     disc1.position.set(0, 10, 0);
     disc1.rotation.set(Math.PI*0.5, 0, 0);
 
@@ -108,7 +107,7 @@ function createObjects(){
     holePath.absarc(0, 0, 12, 0, Math.PI * 2, true);
     disc2_shape.holes.push(holePath);
     var disc2_geometry = new THREE.ExtrudeGeometry(disc2_shape, extrudesettings);
-    disc2 = new THREE.Mesh(disc2_geometry, temporary_material3);
+    disc2 = new THREE.Mesh(disc2_geometry, diffuse_material3);
     disc2.position.set(0, 0, 0);
     disc2.rotation.set(Math.PI*0.5, 0, 0);
 
@@ -118,7 +117,7 @@ function createObjects(){
     holePath.absarc(0, 0, 19, 0, Math.PI * 2, true);
     disc3_shape.holes.push(holePath);
     var disc3_geometry = new THREE.ExtrudeGeometry(disc3_shape, extrudesettings);
-    disc3 = new THREE.Mesh(disc3_geometry, temporary_material4);
+    disc3 = new THREE.Mesh(disc3_geometry, diffuse_material4);
     disc3.position.set(0, -10, 0);
     disc3.rotation.set(Math.PI*0.5, 0, 0);
 
@@ -136,12 +135,12 @@ function createObjects(){
     var klein_geometry = new ParametricGeometry(kleinParam, 25, 25);
 
 
-    elipsoid1 = new THREE.Mesh(elipsoid_geometry, temporary_material5);
+    elipsoid1 = new THREE.Mesh(elipsoid_geometry, diffuse_material5);
     elipsoid1.position.y = 8.5
     elipsoid1.position.z = -1.5
     disc1.add(elipsoid1)
 
-    elipsoid2 = new THREE.Mesh(elipsoid_geometry, temporary_material5);
+    elipsoid2 = new THREE.Mesh(elipsoid_geometry, diffuse_material5);
     elipsoid2.scale.set(1.2, 1.2, 1.2)
     elipsoid2.position.x = 11
     elipsoid2.position.y = -11
@@ -149,20 +148,20 @@ function createObjects(){
     elipsoid2.rotation.y = Math.PI*0.5
     disc2.add(elipsoid2)
 
-    elipsoid3 = new THREE.Mesh(elipsoid_geometry, temporary_material5);
+    elipsoid3 = new THREE.Mesh(elipsoid_geometry, diffuse_material5);
     elipsoid3.scale.set(1.33, 1.33, 1.33)
     elipsoid3.position.x = -22
     elipsoid3.position.z = -2.5
     elipsoid3.rotation.y = Math.PI*0.25
     disc3.add(elipsoid3)
 
-    hyperboloid1 = new THREE.Mesh(hyperboloid_geometry, temporary_material5);
+    hyperboloid1 = new THREE.Mesh(hyperboloid_geometry, diffuse_material5);
     hyperboloid1.scale.set(0.5, 0.5, 0.5)
     hyperboloid1.position.x = 8.5
     hyperboloid1.position.z = -2.75
     disc1.add(hyperboloid1)
 
-    hyperboloid2 = new THREE.Mesh(hyperboloid_geometry, temporary_material5);
+    hyperboloid2 = new THREE.Mesh(hyperboloid_geometry, diffuse_material5);
     hyperboloid2.scale.set(0.55, 0.55, 0.55)
     hyperboloid2.position.y = 15.5
     hyperboloid2.position.z = -2.5
@@ -170,28 +169,28 @@ function createObjects(){
     hyperboloid2.rotation.y = Math.PI*0.5
     disc2.add(hyperboloid2)
 
-    hyperboloid3 = new THREE.Mesh(hyperboloid_geometry, temporary_material5);
+    hyperboloid3 = new THREE.Mesh(hyperboloid_geometry, diffuse_material5);
     hyperboloid3.scale.set(0.6, 0.6, 0.6)
     hyperboloid3.position.x = 22
     hyperboloid3.position.z = -3.25
     hyperboloid3.rotation.y = Math.PI*0.5
     disc3.add(hyperboloid3)
 
-    cone1 = new THREE.Mesh(cone_geometry, temporary_material5);
+    cone1 = new THREE.Mesh(cone_geometry, diffuse_material5);
     cone1.scale.set(1.25, 1.25, 1.25)
     cone1.position.x = -6
     cone1.position.y = -6
     cone1.position.z = -2
     disc1.add(cone1)
 
-    cone2 = new THREE.Mesh(cone_geometry, temporary_material5);
+    cone2 = new THREE.Mesh(cone_geometry, diffuse_material5);
     cone2.scale.set(1.4, 1.4, 1.4)
     cone2.position.y = -15
     cone2.position.z = -2.25
     cone2.rotation.y = Math.PI*0.5
     disc2.add(cone2)
 
-    cone3 = new THREE.Mesh(cone_geometry, temporary_material5);
+    cone3 = new THREE.Mesh(cone_geometry, diffuse_material5);
     cone3.scale.set(1.55, 1.55, 1.55)
     cone3.position.x = -15.5
     cone3.position.y = -15.5
@@ -199,33 +198,33 @@ function createObjects(){
     cone3.rotation.y = Math.PI*0.75
     disc3.add(cone3)
 
-    torus1 = new THREE.Mesh(torus_geometry, temporary_material5);
+    torus1 = new THREE.Mesh(torus_geometry, diffuse_material5);
     torus1.position.x = -8.5
     torus1.position.z = -1.75
     torus1.rotation.y = Math.PI*0.5
     disc1.add(torus1)
 
-    torus2 = new THREE.Mesh(torus_geometry, temporary_material5);
+    torus2 = new THREE.Mesh(torus_geometry, diffuse_material5);
     torus2.scale.set(1.2, 1.2, 1.2)
     torus2.position.x = 15
     torus2.position.z = -2
     disc2.add(torus2)
 
-    torus3 = new THREE.Mesh(torus_geometry, temporary_material5);
+    torus3 = new THREE.Mesh(torus_geometry, diffuse_material5);
     torus3.scale.set(1.33, 1.33, 1.33)
     torus3.position.y = 22
     torus3.position.z = -2
     torus3.rotation.y = Math.PI*0.75
     disc3.add(torus3)
 
-    helix1 = new THREE.Mesh(helix_geometry, temporary_material5);
+    helix1 = new THREE.Mesh(helix_geometry, diffuse_material5);
     helix1.position.x = -1
     helix1.position.y = -8.5
     helix1.position.z = -1.25
     helix1.rotation.y = Math.PI*0.5
     disc1.add(helix1)
 
-    helix2 = new THREE.Mesh(helix_geometry, temporary_material5);
+    helix2 = new THREE.Mesh(helix_geometry, diffuse_material5);
     helix2.scale.set(1.2, 1.2, 1.2)
     helix2.position.x = 10.5
     helix2.position.y = 10.5
@@ -233,27 +232,27 @@ function createObjects(){
     helix2.rotation.y = Math.PI*0.75   
     disc2.add(helix2)
 
-    helix3 = new THREE.Mesh(helix_geometry, temporary_material5);
+    helix3 = new THREE.Mesh(helix_geometry, diffuse_material5);
     helix3.scale.set(1.33, 1.33, 1.33)
     helix3.position.y = -22
     helix3.position.z = -3.5
     disc3.add(helix3)
 
-    ripple1 = new THREE.Mesh(ripple_geometry, temporary_material5);
+    ripple1 = new THREE.Mesh(ripple_geometry, diffuse_material5);
     ripple1.scale.set(0.9, 0.9, 0.9)
     ripple1.position.x = 6
     ripple1.position.y = -6
     ripple1.position.z = -2.25
     disc1.add(ripple1)
 
-    ripple2 = new THREE.Mesh(ripple_geometry, temporary_material5);
+    ripple2 = new THREE.Mesh(ripple_geometry, diffuse_material5);
     ripple2.position.x = -11
     ripple2.position.y = 11
     ripple2.position.z = -2.5
     ripple2.rotation.y = Math.PI*0.75   
     disc2.add(ripple2)
 
-    ripple3 = new THREE.Mesh(ripple_geometry, temporary_material5);
+    ripple3 = new THREE.Mesh(ripple_geometry, diffuse_material5);
     ripple3.scale.set(1.1, 1.1, 1.1)
     ripple3.position.x = 15.5
     ripple3.position.y = 15.5
@@ -261,21 +260,21 @@ function createObjects(){
     ripple3.rotation.y = Math.PI*0.5
     disc3.add(ripple3)
 
-    flatcircle1 = new THREE.Mesh(flatcircle_geometry, temporary_material5);
+    flatcircle1 = new THREE.Mesh(flatcircle_geometry, diffuse_material5);
     flatcircle1.position.x = 6
     flatcircle1.position.y = 6
     flatcircle1.position.z = -2.35
     flatcircle1.rotation.y = Math.PI*0.25
     disc1.add(flatcircle1)
 
-    flatcircle2 = new THREE.Mesh(flatcircle_geometry, temporary_material5);
+    flatcircle2 = new THREE.Mesh(flatcircle_geometry, diffuse_material5);
     flatcircle2.scale.set(1.1, 1.1, 1.1)
     flatcircle2.position.x = -11
     flatcircle2.position.y = -11
     flatcircle2.position.z = -2.6
     disc2.add(flatcircle2)
 
-    flatcircle3 = new THREE.Mesh(flatcircle_geometry, temporary_material5);
+    flatcircle3 = new THREE.Mesh(flatcircle_geometry, diffuse_material5);
     flatcircle3.scale.set(1.2, 1.2, 1.2)
     flatcircle3.position.x = -15.5
     flatcircle3.position.y = 15.5
@@ -283,21 +282,21 @@ function createObjects(){
     flatcircle3.rotation.y = Math.PI*0.5
     disc3.add(flatcircle3)
 
-    klein1 = new THREE.Mesh(klein_geometry, temporary_material5);
+    klein1 = new THREE.Mesh(klein_geometry, diffuse_material5);
     klein1.scale.set(0.25, 0.25, 0.25)
     klein1.position.x = -6
     klein1.position.y = 6
     klein1.position.z = -2.5
     disc1.add(klein1)
 
-    klein2 = new THREE.Mesh(klein_geometry, temporary_material5);
+    klein2 = new THREE.Mesh(klein_geometry, diffuse_material5);
     klein2.scale.set(0.3, 0.3, 0.3)
     klein2.position.x = -15.25
     klein2.position.z = -2.5
     klein2.rotation.y = Math.PI*0.5
     disc2.add(klein2)
 
-    klein3 = new THREE.Mesh(klein_geometry, temporary_material5);
+    klein3 = new THREE.Mesh(klein_geometry, diffuse_material5);
     klein3.scale.set(0.33, 0.33, 0.33)
     klein3.position.x = 15.5
     klein3.position.y = -15.5
@@ -306,6 +305,52 @@ function createObjects(){
     disc3.add(klein3)
 
     scene.add(main_cylinder);
+}
+
+function createMaterials(){
+    diffuse_material1 = new THREE.MeshLambertMaterial({color: 'Gray'});
+    diffuse_material2 = new THREE.MeshLambertMaterial({color: 'Yellow'});
+    diffuse_material3 = new THREE.MeshLambertMaterial({color: 'Red'});
+    diffuse_material4 = new THREE.MeshLambertMaterial({color: 'Blue'});
+    diffuse_material5 = new THREE.MeshLambertMaterial({color: 'Purple'});
+    diffuse_material1.side = THREE.DoubleSide;
+    diffuse_material2.side = THREE.DoubleSide;
+    diffuse_material3.side = THREE.DoubleSide;
+    diffuse_material4.side = THREE.DoubleSide;
+    diffuse_material5.side = THREE.DoubleSide;
+
+    phong_material1 = new THREE.MeshPhongMaterial({color: 'Gray'});
+    phong_material2 = new THREE.MeshPhongMaterial({color: 'Yellow'});
+    phong_material3 = new THREE.MeshPhongMaterial({color: 'Red'});
+    phong_material4 = new THREE.MeshPhongMaterial({color: 'Blue'});
+    phong_material5 = new THREE.MeshPhongMaterial({color: 'Purple'});
+    phong_material1.side = THREE.DoubleSide;
+    phong_material2.side = THREE.DoubleSide;
+    phong_material3.side = THREE.DoubleSide;
+    phong_material4.side = THREE.DoubleSide;
+    phong_material5.side = THREE.DoubleSide;
+
+    cartoon_material1 = new THREE.MeshToonMaterial({color: 'Gray'});
+    cartoon_material2 = new THREE.MeshToonMaterial({color: 'Yellow'});
+    cartoon_material3 = new THREE.MeshToonMaterial({color: 'Red'});
+    cartoon_material4 = new THREE.MeshToonMaterial({color: 'Blue'});
+    cartoon_material5 = new THREE.MeshToonMaterial({color: 'Purple'});
+    cartoon_material1.side = THREE.DoubleSide;
+    cartoon_material2.side = THREE.DoubleSide;
+    cartoon_material3.side = THREE.DoubleSide;
+    cartoon_material4.side = THREE.DoubleSide;
+    cartoon_material5.side = THREE.DoubleSide;
+
+    normal_material1 = new THREE.MeshNormalMaterial({color: 'Gray'});
+    normal_material2 = new THREE.MeshNormalMaterial({color: 'Yellow'});
+    normal_material3 = new THREE.MeshNormalMaterial({color: 'Red'});
+    normal_material4 = new THREE.MeshNormalMaterial({color: 'Blue'});
+    normal_material5 = new THREE.MeshNormalMaterial({color: 'Purple'});
+    normal_material1.side = THREE.DoubleSide;
+    normal_material2.side = THREE.DoubleSide;
+    normal_material3.side = THREE.DoubleSide;
+    normal_material4.side = THREE.DoubleSide;
+    normal_material5.side = THREE.DoubleSide;
 }
 
 function elipsoidParam(u, v, target) {
@@ -421,11 +466,45 @@ function rotateAll(delta_time){
     flatcircle3.rotation.y += Math.PI*0.1*delta*10;
     klein3.rotation.y += Math.PI*0.1*delta*10;
 }
-////////////
-/* UPDATE */
-////////////
-function update(){
+/////////////////////
+/* MATERIAL CHANGE */
+/////////////////////
+function changeMaterials(type){
     'use strict';
+
+    if (type == "diffuse") {
+        main_cylinder.material = diffuse_material1;
+        disc1.material = diffuse_material2;
+        disc2.material = diffuse_material3;
+        disc3.material = diffuse_material4;
+        for (var j = 0; j < objects.length; j++) {
+            objects[j].material = diffuse_material5;
+        }
+    } else if (type == "phong") {
+        main_cylinder.material = phong_material1;
+        disc1.material = phong_material2;
+        disc2.material = phong_material3;
+        disc3.material = phong_material4;
+        for (var j = 0; j < objects.length; j++) {
+            objects[j].material = phong_material5;
+        }
+    } else if (type == "toon") {
+        main_cylinder.material = cartoon_material1;
+        disc1.material = cartoon_material2;
+        disc2.material = cartoon_material3;
+        disc3.material = cartoon_material4;
+        for (var j = 0; j < objects.length; j++) {
+            objects[j].material = cartoon_material5;
+        }
+    } else if (type == "normal") {
+        main_cylinder.material = normal_material1;
+        disc1.material = normal_material2;
+        disc2.material = normal_material3;
+        disc3.material = normal_material4;
+        for (var j = 0; j < objects.length; j++) {
+            objects[j].material = normal_material5;
+        }
+    }
 
 }
 
@@ -467,6 +546,12 @@ function init() {
     up2 = 0;
     up3 = 0;
     press = true;
+
+    objects = [elipsoid1, elipsoid2, elipsoid3, hyperboloid1,
+        hyperboloid2, hyperboloid3, cone1, cone2, cone3, torus1, 
+        torus2, torus3, helix1, helix2, helix3, ripple1, ripple2,
+        ripple3, flatcircle1, flatcircle2, flatcircle3, klein1, klein2, klein3]
+
     // Event listeners for keyboard input and window resize
     window.addEventListener("keydown", onKeyDown);
     window.addEventListener("keyup", onKeyUp);
@@ -553,6 +638,18 @@ function checkPressedKeys() {
         }
         if (pressedKeys.has('d')) {
             directionalLight.visible = !directionalLight.visible;
+        }
+        if (pressedKeys.has('q')) {
+            changeMaterials("diffuse");
+        }
+        if (pressedKeys.has('w')) {
+            changeMaterials("phong");
+        }
+        if (pressedKeys.has('e')) {
+            changeMaterials("toon");
+        }
+        if (pressedKeys.has('r')) {
+            changeMaterials("normal");
         }
     }
 }
